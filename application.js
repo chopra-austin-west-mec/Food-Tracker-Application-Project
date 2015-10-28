6995d8976b4a7298922886af098d3f957b123266
Austin Chopra <archopra100@aol.com>
2015-10-28 12:25:11 -0700

    no message

--- application.js
+++ application.js
@@ -1,9 +1,22 @@
+var dateOfPurchaseReversed = null;
+var expirationDateReversed = null;
+
+var dateOfPurchaseDay = null;
+var dateOfPurchaseMonth = null;
+var dateOfPurchaseYear = null;
+
+var expirationDateDay = null;
+var expirationDateMonth = null;
+var expirationDateYear = null;
+
+var productNameGo = false;
+var descriptionGo = false;
+var dateOfPurchaseGo = false;
+var expirationDateGo = false;
+
 function addProduct(){
     
-    var productNameGo = false;
-    var descriptionGo = false;
-    var dateOfPurchaseGo = false;
-    var expirationDateGo = false;
+    rewriteDate();
     
     var productName = document.getElementById("productName").value;
     var description = document.getElementById("description").value;
@@ -16,6 +29,7 @@ function addProduct(){
     }
     else{
         productNameGo = true;
+        console.log("productNameGo is set to true");
     }
     
     if (description == "") {
@@ -23,6 +37,7 @@ function addProduct(){
     }
     else{
         descriptionGo = true;
+        console.log("descriptionGo is set to true");
     }
     
     if (dateOfPurchase == "") {
@@ -30,6 +45,7 @@ function addProduct(){
     }
     else{
         dateOfPurchaseGo = true;
+        console.log("dateOfPurchaseGo is set to true");
     }
     
     if (expirationDate == "") {
@@ -37,11 +53,13 @@ function addProduct(){
     }
     else{
         expirationDateGo = true;
+        console.log("expirationDateGo is set to true");
+        
     }
     
     
-    if (productNameGo == true && descriptionGo == true && dateOfPurchase == true && expirationDate == true) {
-        
+    if (productNameGo == true && descriptionGo == true && dateOfPurchaseGo == true && expirationDateGo == true) {
+        console.log("it is running")
         var createTable = document.createElement("TR");
         
         var node = document.createElement("TD");                        //First create an TD node
@@ -67,5 +85,36 @@ function addProduct(){
         node.appendChild(textnode);
         createTable.appendChild(node)
         document.getElementById("productListTable").appendChild(createTable);  //Finally append the TD node to the table
+        
+        productNameGo = false;
+        descriptionGo = false;
+        dateOfPurchaseGo = false;
+        expirationDateGo = false;
     }
+}
+
+function rewriteDate() {
+    var dateOfPurchaseDay = dateOfPurchase.substring(8,9);
+    console.log(dateOfPurchaseDay);
+    
+    
+}
+
+function clearValues() {
+    
+    productNameGo = false;
+    descriptionGo = false;
+    dateOfPurchaseGo = false;
+    expirationDateGo = false;
+    
+    dateOfPurchaseReversed = null;
+    expirationDateReversed = null;
+    
+    dateOfPurchaseDay = null;
+    dateOfPurchaseMonth = null;
+    dateOfPurchaseYear = null;
+    
+    expirationDateDay = null;
+    expirationDateMonth = null;
+    expirationDateYear = null;
 }
